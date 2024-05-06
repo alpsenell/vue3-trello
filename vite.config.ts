1650a1d103b7c92a552b499d37641e28248eb208
alpsenell <alpsenel@outlook.com>
2024-05-06 10:12:02 +0300

    Implement trello web app with Vue 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-trello/',
+  base: '',
   plugins: [vue()],
   resolve: {
     alias: [
